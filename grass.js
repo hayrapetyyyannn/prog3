7b647354f72881a72b6806e3cd2d2b5cfd5343ce
hayrapetyyyannn <erik.hayrapetyan4.y@tumo.org>
2023-02-11 12:43:30 +0400

    imp/exp

--- grass.js
+++ grass.js
@@ -1,20 +1,20 @@
-class Grass {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.multiply = 0;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+class Grass  extends LivingCreature{
+    // constructor(x, y) {
+    //     this.x = x;
+    //     this.y = y;
+    //     this.multiply = 0;
+    //     this.directions = [
+    //         [this.x - 1, this.y - 1],
+    //         [this.x, this.y - 1],
+    //         [this.x + 1, this.y - 1],
+    //         [this.x - 1, this.y],
+    //         [this.x + 1, this.y],
+    //         [this.x - 1, this.y + 1],
+    //         [this.x, this.y + 1],
+    //         [this.x + 1, this.y + 1]
+    //     ];
 
-    }
+    // }
     chooseCell(character) {
         var found = [];
         for (var i in this.directions) {
--- grassEater.js
+++ grassEater.js
@@ -1,33 +1,11 @@
-class GrassEater {
+class GrassEater extends LivingCreature{
     constructor(x, y, index) {
-        this.x = x;
-        this.y = y;
-        this.energy = 8;
-        this.index = index;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
+    super(x,y,index)
+    this.energy=8;
     }
 
-    chooseCell(character) {
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+    chooseCell(ch) {
+        return super.chooseCell(ch)
     }
     getNewCoordinates() {
         this.directions = [
