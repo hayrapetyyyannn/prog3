69ef95680066e6cb25ea09d10454e0597d83663c
hayrapetyyyannn <erik.hayrapetyan4.y@tumo.org>
2023-02-18 12:40:00 +0400

    ls4

--- express.js
+++ express.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var app = express();
-app.use(express.static("modules")); 
 app.get("/", function(req, res){
-res.redirect("index.html");
+res.send("Hello world");
 });
 app.listen(3000, function(){
 console.log("Example is running on port 3000");
